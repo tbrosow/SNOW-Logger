663f37de17d70157921e527acabbcc25625a9733
Torsten Brosow <torsten.brosow@gmail.com>
2020-06-26 08:06:00 +1000

    no message

---
+++ getlog.js
@@ -0,0 +1,311 @@
+/*
+	Change Username line 14
+	Change Password line 15
+	Change Logfile  line 18
+*/
+
+
+const fs = require('fs');
+const readline = require('readline');
+var url = require('url');
+
+var myArgs = process.argv.slice(2);
+var myFile = process.argv[1];
+
+var host = "";
+var username = "";
+var password;
+var verbose = false;
+
+// let data = username + ":" + password;
+let configFile = "getlog.json";
+let logFile = __dirname + "/getlog.log";
+// console.log("AUTH: [" + data + "] ");
+let auth;
+
+const https = require('https')
+const options = {
+  hostname: host,
+  port: 443,
+  // path: '/api/now/table/syslog?sysparm_limit=20000&sysparm_query='+encodeURIComponent(query), 
+  method: 'GET',
+  headers: {
+        // Authorization: auth
+  }
+}
+
+if (process.argv.length <= 2) {
+	console.log("Usage: node getlog.js");
+	console.log("\t-v Verbose Mode")
+	console.log("\t-c[ConfigFile]")
+	console.log("\t-l[LogFile]")
+	console.log("\n\tExample: node getlog.js -csampleconfig.json -v")
+	return 
+}
+process.argv.forEach(function (param) {
+	// console.log(__dirname + " " + param)
+  if (param.match(/^-c/)) {
+  	configFile = param.substring(2);
+  }
+  if (param.match(/^-v/)) {
+  	verbose = true;
+  }
+  if (param.match(/^-l/)) {
+  	logFile = param.substring(2);
+  }
+});
+
+
+fs.readFile(configFile, function(err, data) {
+	if (err) {
+		console.log(err);
+		console.log("Configfile: " + configFile);
+	}
+	else {
+		try {
+			let confObj = JSON.parse(data);
+			_log("DATA: " + JSON.stringify(confObj, null, 4));
+			_log("instance: " + confObj.instance);	
+			if (confObj.logfile)
+				logFile = confObj.logfile;
+			if (confObj.username)
+				username = confObj.username;
+			host += confObj.instance + ".service-now.com";
+			if (confObj.auth) {
+				auth = confObj.auth;
+				let cf = {
+					auth: auth,
+					instance: confObj.instance,
+					logfile: logFile,
+					username: username
+				}
+				writeConfig(cf);
+				options.headers.Authorization = auth;
+				options.hostname = host;
+
+				_log("CONNECT:\n"+JSON.stringify(options))
+
+				getUserID(getUserIDResponse);
+			} else if (confObj.username && confObj.password) {
+				_log("username: " + confObj.username);	
+				_log("password: " + confObj.password);	
+				let userpw = username + ":" + password;
+				auth = "Basic " + Buffer.from(userpw).toString('base64');
+				let cf = {
+					auth: auth,
+					instance: confObj.instance,
+					logfile: logFile,
+					username: username
+				}
+
+				writeConfig(cf);
+			}
+			_log("AUTH: [" + auth + "] ");
+		} catch (e) {
+			console.log("Error reading Config file " + configFile + "\n" + e)
+		}
+		
+	}
+});
+
+
+return
+
+function _log(_text) {
+	if (verbose)
+		console.log(_text);
+}
+
+function writeConfig(_obj) {
+	fs.writeFile(configFile, JSON.stringify(_obj, null, 4), function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+	    console.log(`Saved Configfile: ${configFile}\n ${JSON.stringify(_obj, null, 4)} - File saved ...\n\n`);
+	});
+}
+// let buff = new Buffer(data);
+// let auth = "Basic " + buff.toString('base64');
+
+// var auth = "Basic " + String(username + ":" + password).toString('base64');
+
+
+
+
+function getUserIDResponse(resp) {
+	// console.log("getUserIDResponse: " + resp.sys_id);
+	if (resp != false)
+		getMyBookmarks(resp.sys_id, getMyBookmarksResponse);
+
+}
+
+function getMyBookmarksResponse(resp) {
+	console.log("Instance: " + host)
+	console.log("Logfile: " + logFile)
+	console.log("\n\n")
+	for (var idx=0; idx<resp.length; idx++) {
+		console.log("[" + idx + "] " + resp[idx].title);
+	}
+	const rl = readline.createInterface({
+	  input: process.stdin,
+	  output: process.stdout
+	});
+
+	rl.question('WTF you want to see? ', (answer) => {
+  		// TODO: Log the answer in a database
+  		var hit = false;
+  		console.log(`OK, cool: ${answer}`);
+
+  		rl.close();
+
+  		for (var idx=0; idx<resp.length; idx++) {
+			// console.log(answer + " [" + idx + "] " + resp[idx].title);
+			if (answer == idx) {
+				// console.log("HIT");
+				hit = true;
+				// console.log("HIT: "+resp[idx].url);
+				var url_parts = url.parse(resp[idx].url, true);
+				// console.log("HIT2: "+JSON.stringify(url_parts, null, 4));
+				var query = url_parts.query.sysparm_query;
+				getLogs(query);
+			}
+		}
+
+		if (!hit) {
+			console.log("NO HIT");
+			setTimeout(function() {
+				getUserID(getUserIDResponse);
+			}, 2000);
+		}
+	});
+	//sleep(1000);
+}
+
+function getUserID(callback) {
+	let obj = {};
+	options.path = '/api/now/table/sys_user?sysparm_query='+encodeURIComponent("user_name=" + username);
+	const req = https.request(options, res => {
+		let data = '';
+
+		res.on('data', (chunk) => {
+	    	data += chunk;
+	  	});
+
+	  	res.on('end', () => {
+	  		// console.log("Email: " + data + "\n\n");
+	  		obj = JSON.parse(data);
+	  		// console.log("Email: " + obj.result.length + "\n\n");
+	  		// console.log("Email: " + obj.result[0].email + "\n\n");
+	  		if (obj.result.length == 1)
+	  			return callback(obj.result[0]);
+	  		else {
+	  			console.log("Could not log in, check config file ...\n\n");
+	  			return false;
+	  		}
+	  	});
+	});
+
+	req.on('error', error => {
+	  console.error(error)
+	})
+
+	req.end()
+}
+
+function getMyBookmarks(_id, callback) {
+	
+	let obj = {};
+	options.path = '/api/now/table/sys_ui_bookmark?sysparm_query='+encodeURIComponent("user=" + _id + "^urlLIKEsyslog");
+	const req = https.request(options, res => {
+		let data = '';
+
+		res.on('data', (chunk) => {
+	    	data += chunk;
+	  	});
+
+	  	res.on('end', () => {
+	  		obj = JSON.parse(data);
+	  		// console.log("BOOKMARKS" + JSON.stringify(obj, null, 4));
+	  		return callback(obj.result);
+	  	});
+	});
+
+	req.on('error', error => {
+	  console.error(error)
+	})
+
+	req.end()
+}
+
+function getLogs(_query) {
+	// console.log("getLogs Query: " + _query)
+	options.path = '/api/now/table/syslog?sysparm_limit=200000&sysparm_query='+encodeURIComponent(_query);
+	const req = https.request(options, res => {
+		let data = '';
+		let msg = [];
+	  	// console.log(`statusCode: ${res.statusCode}`)
+
+		res.on('data', (chunk) => {
+	    	data += chunk;
+	  	});
+
+	  	res.on('end', () => {
+	    //process.stdout.write(d)
+	    var obj = JSON.parse(data)
+	    // console.log("getLogs LEN: " + obj.result.length)
+	    for (var idx=0; idx<obj.result.length; idx++) {
+			// console.log(obj.result[idx].message); 
+			msg.push(obj.result[idx].message);   	
+	    }
+	    msg.sort();
+
+	    fs.readFile(logFile, function(err, data) {
+	    	let len = 0;
+	    	// console.log(`read len ${data.length}`);
+	    	if (err) {
+	    		console.log(err)
+	    		console.log(msg.join("\n"));
+	    		msg.reverse();
+	    	
+		    	fs.writeFile(logFile, msg.join("\n"), function(err) {
+				    if(err) {
+				        return console.log(err);
+				    }
+				    console.log(`statusCode: ${res.statusCode} - File saved ...`);
+				});
+	    	} else {
+		    	let dataNew = msg.join("\n");
+		    	if (data.length != dataNew.length) {
+		    		console.log("Ups - need refresh ... ")
+
+		    		console.log(msg.join("\n"));
+		    		msg.reverse();
+		    	
+			    	fs.writeFile(logFile, msg.join("\n"), function(err) {
+					    if(err) {
+					        return console.log(err);
+					    }
+					    _log(`statusCode: ${res.statusCode} - File saved ...`);
+					});
+					
+				}
+			}
+	    });
+	     
+		setTimeout(function() {
+			getLogs(_query)
+		}, 3000);	
+	  })
+	})
+
+	req.on('error', error => {
+	  console.error(error)
+	})
+
+	req.end()
+
+	
+}
+
+
+
